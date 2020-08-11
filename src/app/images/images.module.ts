acbeea5dc7a84f5bc0aa7871f17e9877abf0c718
Akhtem Ways <ahty.study@bk.ru>
2020-08-11 21:27:17 +0300

    initial commit

---
+++ src/app/images/images.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ImagesRoutingModule } from './images-routing.module';
+import { ImagesComponent } from './images/images.component';
+
+@NgModule({
+  declarations: [ImagesComponent],
+  imports: [CommonModule, ImagesRoutingModule],
+  exports: [ImagesComponent],
+})
+export class ImagesModule {}
